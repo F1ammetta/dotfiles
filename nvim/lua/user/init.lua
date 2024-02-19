24d51f7a4ca276bb66c3638de76ef6e79b75c3ea
Sergio Piñeres <sagdpr03@gmail.com>
2024-02-19 08:17:22 -0500

    :D

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,6 +1,14 @@
 vim.cmd('au BufNewFile,BufRead *.templ set filetype=templ')
 -- vim.filetype.add({extensions = {gotempl = "go"}})
 
+local notify = vim.notify
+vim.notify = function(msg, ...)
+    if msg:match("warning: multiple different client offset_encodings") then
+        return
+    end
+
+    notify(msg, ...)
+end
 
 return {
     options = {
