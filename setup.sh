199df7c15283d68fdee845a127d603f526648617
Sergio <sagdpr03@gmail.com>
2024-06-27 12:56:00 -0500

    wifi menu added

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
 dotfiles_dir=$(pwd)
 
-#install zsh plugins
+# Install zsh plugins
 echo "Installing zsh plugins ..."
 cd .oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
@@ -20,7 +20,7 @@ cd .fonts
 cp $dotfiles_dir/.fonts/* .
 fc-cache -f -v
 
-#create symlinks
+# Create symlinks
 cd $HOME
 ln -s $dotfiles_dir/.zshrc
 ln -s $dotfiles_dir/.oh-my-zsh
@@ -42,21 +42,34 @@ ln -s $dotfiles_dir/.config/nvim
 ln -s $dotfiles_dir/.config/picom.conf
 ln -s $dotfiles_dir/.config/rofi
 
-#install packages
+# Install packages
 echo "Installing packages ..."
 pacman -S $(cat $dotfiles_dir/packages.txt | tr '\n' ' ')
 
-# Install paru
-echo "Installing the mighty AUR helper (paru)..."
+# Install rofi wifi menu
 cd $HOME
 if [ ! -d "repos" ]; then
     mkdir repos
 fi
+cd repos
+git clone https://github.com/isaif/polybar-wifi-ramp-icons
+cd polybar-wifi-ramp-icons
+if [ ! -d "$HOME/.local/bin" ]; then
+    mkdir $HOME/.local/bin
+fi
+ln -s polybar-wifi-ramp-icons $HOME/.local/bin/wifi-menu
+
+
+# Install paru
+echo "Installing the mighty AUR helper (paru)..."
+cd $HOME
 cd repos
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
 paru -S $(cat $dotfiles_dir/paru.txt | tr '\n' ' ')
+
+chmod +s /usr/bin/brillo
 
 echo "Done!"
